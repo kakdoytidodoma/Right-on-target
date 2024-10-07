7c8b3bbc4687feef9d3be2862ce89ada21b289e8
kakdoytidodoma <bigballsoregor@gmail.com>
2024-10-07 16:40:53 +0300

    добавлен второй экран

--- Right on target/Right on target/SecondViewController.swift
+++ Right on target/Right on target/SecondViewController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBAction func hideCurentScene(){
+        dismiss(animated: true, completion: nil)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("loadView SecondViewController")
+       
     }
-    */
-
+    
+    
+    override func loadView() {
+        super.loadView()
+        print ("loadView SecondViewController")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print ("viewWillAppear SecondViewController")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print ("viewDidAppear SecondViewController")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print ("viewWillDisappear SecondViewController")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print ("viewDidDisappear SecondViewController")
+    }
 }
--- Right on target/Right on target/ViewController.swift
+++ Right on target/Right on target/ViewController.swift
@@ -12,15 +12,24 @@ class ViewController: UIViewController {
     @IBOutlet var label: UILabel!
     
     var number = 0
-    var round = 0
+    var round = 1
     var points = 0
     
+    lazy var secondViewController: SecondViewController = getSecondViewController()
+    
+    private func getSecondViewController() -> SecondViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        return storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+    }
+    
+    
+    
+    @IBAction func showNextScreen() {
+        present(secondViewController, animated: true , completion: nil)
+    }
+    
     @IBAction func checkNumber() {
-        if round == 0 {
-            number = Int.random(in: 1...50)
-            label.text = String(number)
-            round = 1
-        } else {
+       
             let numSlider = Int(slider.value.rounded())
             if numSlider > number {
                 points += 50 - numSlider + number
@@ -40,7 +49,6 @@ class ViewController: UIViewController {
             }
             number = Int.random(in: 1...50)
             label.text = String(number)
-        }
     }
     
     
@@ -49,9 +57,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print (viewDidLoad)
+        number = Int.random(in: 1...50)
+        label.text = String(number)
         
+    }
+    
+    override func loadView () {
+        super.loadView()
+        print("loadView")
+        let versionLabel = UILabel(frame: CGRect(x: 20, y: 10, width: 200, height: 20))
+        versionLabel.text = "Version: 1.1"
+        view.addSubview(versionLabel)
         
-        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
     }
 
 
